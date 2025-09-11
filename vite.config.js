e5ed706f917bae97293a2aad573d6f3151c43505
Your Name <your_email@example.com>
2025-09-11 17:46:16 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -14,5 +14,5 @@ export default defineConfig({
   build: {
     outDir: "dist",
   },
-  base: "/", // 🔑 important for Vercel routing
+  base: process.env.VITE_BASE_PATH || "/beyaa-frontend-v1", // 🔑 important for Vercel routing
 });
